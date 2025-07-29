d41f6bd7231a44785f53b1c3ebf6c840ee1c8a51
rahulprasad0710 <raulshah2021@hotmail.com>
2025-07-30 01:55:45 +0545

    feat: permission FE complete

--- frontend/src/pages/RolesAndPermission/Permission.tsx
+++ frontend/src/pages/RolesAndPermission/Permission.tsx
@@ -5,6 +5,7 @@ import {
 
 import type { IPermissionGroupResponse } from "@/types/config.types";
 import Skeleton from "@/components/common/Skeleton";
+import { TrashBinIcon } from "@/icons";
 import { useState } from "react";
 
 const Permission = () => {
@@ -47,8 +48,8 @@ const Permission = () => {
                 </h2>
             </div>
             <div className='border-t border-gray-100 p-4 dark:border-gray-800 sm:p-6'>
-                <div x-data="{ activeTab: 'overview' }">
-                    <div className='rounded-t-xl border border-gray-200 p-3 dark:border-gray-800'>
+                <div>
+                    <div className='rounded-t-md border border-gray-200 p-3 dark:border-gray-800'>
                         <nav className='flex gap-2 overflow-x-auto rounded-lg bg-gray-100 p-1 dark:bg-gray-900 [&amp;::-webkit-scrollbar-thumb]:rounded-full [&amp;::-webkit-scrollbar-thumb]:bg-gray-200 dark:[&amp;::-webkit-scrollbar-thumb]:bg-gray-600 [&amp;::-webkit-scrollbar-track]:bg-white dark:[&amp;::-webkit-scrollbar-track]:bg-transparent [&amp;::-webkit-scrollbar]:h-1.5'>
                             {isFetching
                                 ? Array.from({ length: 8 }).map((_, index) => (
@@ -78,14 +79,89 @@ const Permission = () => {
                                   )}
                         </nav>
                     </div>
-                    <div className='rounded-b-xl border border-t-0 border-gray-200 p-6 pt-4 dark:border-gray-800'>
-                        <div x-show="activeTab === 'customers'">
+                    <div className='rounded-b-md border border-t-0 border-gray-200 p-6 pt-4 dark:border-gray-800'>
+                        <div>
                             <h3 className='mb-1 text-xl font-medium text-gray-800 dark:text-white/90'>
                                 {PermissionGroupDetails?.data.displayName}
                             </h3>
                             <p className='text-sm text-gray-500 dark:text-gray-400'>
                                 {PermissionGroupDetails?.data.description}
                             </p>
+                        </div>
+                        <div className='border border-t border-gray-200 py-4 mt-4 dark:border-gray-800 sm:p-6'>
+                            <table className='min-w-full'>
+                                <thead className='border-gray-100 border-y bg-gray-50 dark:border-gray-800 dark:bg-gray-900'>
+                                    <tr>
+                                        <th className='px-6 py-3 whitespace-nowrap'>
+                                            <div className='flex items-center'>
+                                                <p className='font-medium  text-slate-800 text-theme-xs dark:text-gray-400'>
+                                                    Name
+                                                </p>
+                                            </div>
+                                        </th>
+                                        <th className='px-6 py-3 whitespace-nowrap'>
+                                            <div className='flex items-center'>
+                                                <p className='font-medium text-gray-500 text-theme-xs dark:text-gray-400'>
+                                                    Description
+                                                </p>
+                                            </div>
+                                        </th>
+                                        <th className='px-6 py-3 whitespace-nowrap'>
+                                            <div className='flex items-center'>
+                                                <p className='font-medium text-gray-500 text-theme-xs dark:text-gray-400'>
+                                                    Status
+                                                </p>
+                                            </div>
+                                        </th>
+                                        <th className='px-6 py-3 whitespace-nowrap'>
+                                            <div className='flex items-center justify-center'>
+                                                <button className='font-medium text-gray-500 text-theme-xs dark:text-gray-400 '>
+                                                    Action
+                                                </button>
+                                            </div>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className='divide-y divide-gray-100 dark:divide-gray-800'>
+                                    {PermissionGroupDetails?.data.permissions?.map(
+                                        (item) => (
+                                            <tr key={item.id}>
+                                                <td className='px-6 py-3 whitespace-nowrap'>
+                                                    <div className='flex items-center'>
+                                                        <p className='text-gray-700 text-theme-sm dark:text-gray-400'>
+                                                            {item.displayName}
+                                                        </p>
+                                                    </div>
+                                                </td>
+                                                <td className='px-6 py-3 whitespace-nowrap'>
+                                                    <div className='flex items-center'>
+                                                        <p className='text-gray-700 text-theme-sm dark:text-gray-400'>
+                                                            {item.description}
+                                                        </p>
+                                                    </div>
+                                                </td>
+                                                <td className='px-6 py-3 whitespace-nowrap'>
+                                                    <div className='flex items-center'>
+                                                        <p className='bg-success-50 text-theme-xs text-success-600 dark:bg-success-500/15 dark:text-success-500 rounded-full px-2 py-0.5 font-medium'>
+                                                            {item.isActive
+                                                                ? "Active"
+                                                                : "Inactive"}
+                                                            {}
+                                                        </p>
+                                                    </div>
+                                                </td>
+                                                <td className='px-6 py-3 whitespace-nowrap'>
+                                                    <div className='flex items-center justify-center'>
+                                                        <button className='cursor-pointer hover:text-blue-700'>
+                                                            <TrashBinIcon />
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        )
+                                    )}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
